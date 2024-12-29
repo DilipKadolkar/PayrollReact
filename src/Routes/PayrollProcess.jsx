8eeb6d045d9b8bcb025284305ad6c6f4a1f351ab
dilip.kadolkar@gmail.com <dilip.kadolkar@gmail.com>
2024-12-29 15:46:32 +0530

    card created for payroll

--- src/Routes/PayrollProcess.jsx
+++ src/Routes/PayrollProcess.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Card from '../components/Card'; // Make sure this path is correct based on your project structure
 
 export default function PayrollProcess() {
-    return ( <
-        div className = "displayContentWhenNavbar" > PayrollProcess < /div>
-    )
+  return (
+    <div className="displayContentWhenNavbar">
+      <Card title="Add employe"></Card>
+      <Card title="Salary Breakdown"></Card>
+      <Card title="Tax Information"></Card>
+    </div>
+  );
 }
+
