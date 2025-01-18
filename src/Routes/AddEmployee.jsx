ea43b4f31935c745bbe7576349723f7659e9e0b5
dilip.kadolkar@gmail.com <dilip.kadolkar@gmail.com>
2025-01-18 12:41:45 +0530

    add employee changes

--- src/Routes/AddEmployee.jsx
+++ src/Routes/AddEmployee.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCalendar, faPhone, faEnvelope, faIdCard, faBuilding, faUserTie, faVenusMars, faClock } from '@fortawesome/free-solid-svg-icons';
 import styles from './AddEmployee.module.css'; // Import the CSS module
 
 export default function AddEmployee() {
     const [selectedCompany, setSelectedCompany] = useState('');
     const [employeeName, setEmployeeName] = useState('');
+    const [spouseName, setSpouseName] = useState('');
+    const [fatherName, setFatherName] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+    const [joiningDate, setJoiningDate] = useState('');
+    const [gender, setGender] = useState('');
+    const [contactNumber, setContactNumber] = useState('');
+    const [probationPeriod, setProbationPeriod] = useState('');
+    const [aadharNumber, setAadharNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [employeeType, setEmployeeType] = useState('');
 
     const handleFormSubmit = async(event) => {
         event.preventDefault();
@@ -14,13 +26,36 @@ export default function AddEmployee() {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ company: selectedCompany, name: employeeName }),
+                    body: JSON.stringify({
+                        company: selectedCompany,
+                        name: employeeName,
+                        spouseName,
+                        fatherName,
+                        birthDate,
+                        joiningDate,
+                        gender,
+                        contactNumber,
+                        probationPeriod,
+                        aadharNumber,
+                        email,
+                        employeeType
+                    }),
                 });
 
                 if (response.ok) {
                     alert('Employee added successfully');
                     setSelectedCompany('');
                     setEmployeeName('');
+                    setSpouseName('');
+                    setFatherName('');
+                    setBirthDate('');
+                    setJoiningDate('');
+                    setGender('');
+                    setContactNumber('');
+                    setProbationPeriod('');
+                    setAadharNumber('');
+                    setEmail('');
+                    setEmployeeType('');
                 } else {
                     alert('Error adding employee');
                 }
@@ -36,6 +71,8 @@ export default function AddEmployee() {
     const companies = ['Company A', 'Company B', 'Company C']; // Example companies
 
     return ( <
+            div className = { styles.addEmployeeWrapper } >
+            <
             div className = { styles.displayContentWhenNavbar } >
             <
             div className = { styles.addEmployeeContainer } >
@@ -44,6 +81,11 @@ export default function AddEmployee() {
             form onSubmit = { handleFormSubmit }
             className = { styles.addEmployeeForm } >
             <
+            div className = { styles.inputGroup } >
+            <
+            FontAwesomeIcon icon = { faBuilding }
+            className = { styles.icon }
+            /> <
             select value = { selectedCompany }
             onChange = {
                 (e) => setSelectedCompany(e.target.value)
@@ -56,25 +98,184 @@ export default function AddEmployee() {
                 value = { company } > { company } < /option>
             ))
         } <
-        /select> {
-    selectedCompany && ( <
-        >
+        /select> < /
+        div > <
+        div className = { styles.gridContainer } >
         <
-        input type = "text"
-        placeholder = "Enter employee name"
-        value = { employeeName }
-        onChange = {
-            (e) => setEmployeeName(e.target.value)
-        }
-        className = { styles.employeeInput }
-        /> <
-        button type = "submit"
-        className = { styles.submitButton } > Submit < /button> < /
-        >
-    )
-} <
-/form> < /
-div > <
-    /div>
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faUser }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter employee name"
+    value = { employeeName }
+    onChange = {
+        (e) => setEmployeeName(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faUser }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter spouse name"
+    value = { spouseName }
+    onChange = {
+        (e) => setSpouseName(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faUser }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter father name"
+    value = { fatherName }
+    onChange = {
+        (e) => setFatherName(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faCalendar }
+    className = { styles.icon }
+    /> <
+    label className = { styles.label } > Birth Date < /label> <
+    input type = "date"
+    value = { birthDate }
+    onChange = {
+        (e) => setBirthDate(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faCalendar }
+    className = { styles.icon }
+    /> <
+    label className = { styles.label } > Joining Date < /label> <
+    input type = "date"
+    value = { joiningDate }
+    onChange = {
+        (e) => setJoiningDate(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.genderContainer } >
+        <
+        FontAwesomeIcon icon = { faVenusMars }
+    className = { styles.icon }
+    /> <
+    label >
+        <
+        input type = "radio"
+    value = "Male"
+    checked = { gender === 'Male' }
+    onChange = {
+        (e) => setGender(e.target.value)
+    }
+    />
+    Male <
+        /label> <
+    label >
+        <
+        input type = "radio"
+    value = "Female"
+    checked = { gender === 'Female' }
+    onChange = {
+        (e) => setGender(e.target.value)
+    }
+    />
+    Female <
+        /label> < /
+        div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faPhone }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter contact number"
+    value = { contactNumber }
+    onChange = {
+        (e) => setContactNumber(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faClock }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter probation period"
+    value = { probationPeriod }
+    onChange = {
+        (e) => setProbationPeriod(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faIdCard }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter aadhar number"
+    value = { aadharNumber }
+    onChange = {
+        (e) => setAadharNumber(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faEnvelope }
+    className = { styles.icon }
+    /> <
+    input type = "email"
+    placeholder = "Enter email"
+    value = { email }
+    onChange = {
+        (e) => setEmail(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        div className = { styles.inputGroup } >
+        <
+        FontAwesomeIcon icon = { faUserTie }
+    className = { styles.icon }
+    /> <
+    input type = "text"
+    placeholder = "Enter employee type"
+    value = { employeeType }
+    onChange = {
+        (e) => setEmployeeType(e.target.value)
+    }
+    className = { styles.employeeInput }
+    /> < /
+    div > <
+        /div> <
+    button type = "submit"
+    className = { styles.submitButton } > Submit < /button> < /
+        form > <
+        /div> < /
+        div > <
+        /div>
 );
 }
